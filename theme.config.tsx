31dc12c428cfdba22b09362675f7e76b764c5d95
Vincent Chen <okcdz@diverse.space>
2023-01-08 23:28:25 +0800

    feat: update basic information

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>PoloDB</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/vincentdchan/PoloDB",
   },
   chat: {
-    link: 'https://discord.com',
+    link: "https://discord.com",
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/vincentdchan/polodb-docs",
   footer: {
-    text: 'Nextra Docs Template',
+    text: `Copyright © ${new Date().getFullYear()} Vincent Chen`,
   },
-}
+};
 
-export default config
+export default config;
