89bb954b33f9da839dd5fae75383731c8760ad2b
Vincent Chen <okcdz@diverse.space>
2023-01-08 23:36:03 +0800

    feat: add GA script

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,24 @@
 import React from "react";
 import Link from "next/link";
+import Script from "next/script";
 
 export default function () {
   return (
     <div>
       Hello <Link href="/docs">docs</Link>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-6BLWHTFTEP"
+      ></Script>
+      <Script>
+        {`
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-6BLWHTFTEP');
+  `}
+      </Script>
     </div>
   );
 }
