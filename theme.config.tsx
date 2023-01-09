5b2f8f802c0f7ddc2f345e84d6474856c983979c
Vincent Chen <okcdz@diverse.space>
2023-01-09 14:56:02 +0800

    feat: update config for docs

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,8 +1,38 @@
 import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
+import Head from "next/head";
+import Script from "next/script";
+
+function GAScript() {
+  return (
+    <>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-6BLWHTFTEP"
+      ></Script>
+      <Script>
+        {`
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-6BLWHTFTEP');
+  `}
+      </Script>
+    </>
+  );
+}
 
 const config: DocsThemeConfig = {
-  logo: <span>PoloDB</span>,
+  logo: (
+    <>
+      <span>PoloDB</span>
+      <Head>
+        <link rel="shortcut icon" href="/static/favicon.ico" />
+        <GAScript />
+      </Head>
+    </>
+  ),
   project: {
     link: "https://github.com/vincentdchan/PoloDB",
   },
@@ -12,6 +42,11 @@ const config: DocsThemeConfig = {
   docsRepositoryBase: "https://github.com/vincentdchan/polodb-docs",
   footer: {
     text: `Copyright © ${new Date().getFullYear()} Vincent Chen`,
+  },
+  useNextSeoProps() {
+    return {
+      titleTemplate: "%s – PoloDB",
+    };
   },
 };
 
